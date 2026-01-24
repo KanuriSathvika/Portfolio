32c41c1399a868a59e83fc33ed86f1de62bdd9eb
Sathvika <Sathvika.Kanuri@data-smith.ai>
2026-01-24 19:35:40 +0530

    Fix base path for correct repository name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sathvikasprofilepages/',
+  base: '/Portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
